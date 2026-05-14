3566eb4d8f9816ce1555ccd1bd2e23d6553972aa
Logan Devine <logan@zirco.dev>
2026-05-14 11:55:16 -0700

    change formatting of hidden events and unknown messages, and prettierrc

--- web/src/ui/timeline/content/HiddenEvent.tsx
+++ web/src/ui/timeline/content/HiddenEvent.tsx
@@ -1,5 +1,6 @@
 // gomuks - A Matrix client written in Go.
 // Copyright (C) 2024 Tulir Asokan
+// Copyright (C) 2026 Logan Devine
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -13,10 +14,39 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import { MouseEvent } from "react"
+import { EventID } from "../../../api/types/mxtypes.ts"
+import { useRoomContext } from "../../roomview/roomcontext.ts"
+import { jumpToEventInView } from "../../util/jumpToEvent.tsx"
 import EventContentProps from "./props.ts"
 
 const HiddenEvent = ({ event }: EventContentProps) => {
-	return <code>{`{ "type": "${event.type}" }`}</code>
+	const roomCtx = useRoomContext()
+	const jumpToOnClick = (event_id: EventID) => (evt: MouseEvent<HTMLAnchorElement>) => {
+		evt.preventDefault()
+		jumpToEventInView(roomCtx, event_id, evt.currentTarget.closest(".timeline-list"))
+	}
+	const makeEventURI = (e: EventID) =>
+		`matrix:roomid/${encodeURIComponent(roomCtx.store.roomID.slice(1))}/e/${encodeURIComponent(e.slice(1))}`
+
+	const renderEventLink = (event_id: EventID) => (
+		<a key={event_id} href={makeEventURI(event_id)} onClick={jumpToOnClick(event_id)}>
+			{event_id}
+		</a>
+	)
+
+	if (event.type === "m.room.redaction") {
+		return <code>m.room.redaction {renderEventLink(event.content.redacts)}</code>
+	} else if (event.type === "m.reaction") {
+		return (
+			<code>
+				m.reaction event_id={renderEventLink(event.content["m.relates_to"].event_id)}, key=
+				{event.content["m.relates_to"].key}
+			</code>
+		)
+	} else {
+		return <code>{`${event.type}`}</code>
+	}
 }
 
 export default HiddenEvent
