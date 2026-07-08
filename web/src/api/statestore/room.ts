61081e1faf6990f9db82e15f1fdbe4c4f9777d4e
Logan Devine <logan@zirco.dev>
2026-07-08 11:10:01 -0700

    feat: don't select matrix.org as the 'populated' via

--- web/src/api/statestore/room.ts
+++ web/src/api/statestore/room.ts
@@ -393,7 +393,7 @@ export class RoomStateStore {
 		const servers = Array.from(memberCount.entries())
 		servers.sort(([, a], [, b]) => b - a)
 		for (const [serverName] of servers) {
-			if (serverName !== ownServerName && serverName !== powerServer) {
+			if (serverName !== ownServerName && serverName !== powerServer && serverName !== "matrix.org") {
 				vias.push(serverName)
 				break
 			}
