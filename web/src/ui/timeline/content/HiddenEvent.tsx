74fcf0624c0db4db98f65a01d1a829275ffbbf21
Logan Devine <logan@zirco.dev>
2026-07-09 12:37:39 -0700

    web/timeline: add links to referenced events in reactions, redactions, and replacements

--- web/src/ui/timeline/content/HiddenEvent.tsx
+++ web/src/ui/timeline/content/HiddenEvent.tsx
@@ -1,5 +1,6 @@
 // gomuks - A Matrix client written in Go.
 // Copyright (C) 2024 Tulir Asokan
+// Copyright (C) 2026 Logan Devine
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -13,10 +14,46 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import { MouseEvent } from "react"
+import { EventID } from "../../../api/types/mxtypes.ts"
+import { useRoomContext } from "../../roomview/roomcontext.ts"
+import { jumpToEventInView } from "../../util/jumpToEvent.tsx"
 import EventContentProps from "./props.ts"
 
 const HiddenEvent = ({ event }: EventContentProps) => {
-	return <code>{`{ "type": "${event.type}" }`}</code>
+	const roomCtx = useRoomContext()
+	const jumpToOnClick = (event_id: EventID) => (evt: MouseEvent<HTMLAnchorElement>) => {
+		evt.preventDefault()
+		jumpToEventInView(roomCtx, event_id, evt.currentTarget.closest(".timeline-list"))
+	}
+	const makeEventURI = (e: EventID) =>
+		`matrix:roomid/${encodeURIComponent(roomCtx.store.roomID.slice(1))}/e/${encodeURIComponent(e.slice(1))}`
+
+	const renderEventLink = (event_id: EventID, contents: string = event_id) => (
+		<a key={event_id} href={makeEventURI(event_id)} onClick={jumpToOnClick(event_id)}>
+			{contents}
+		</a>
+	)
+
+	// check for a field within content so that redacted reactions/redactions don't throw
+	if (event.type === "m.room.redaction" && event.content.redacts) {
+		return (
+			<code>
+				m.room.redaction {"{"} redacts: {renderEventLink(event.content.redacts)} {"}"}
+			</code>
+		)
+	} else if (event.type === "m.reaction" && event.content["m.relates_to"]) {
+		return (
+			<code>
+				m.reaction {renderEventLink(event.content["m.relates_to"].event_id, "[m.annotation]")} {"{"} key:{" "}
+				{event.content["m.relates_to"].key} {"}"}
+			</code>
+		)
+	} else if (event.type === "m.room.message" && event.content["m.relates_to"]?.rel_type === "m.replace") {
+		return <code>m.room.message {renderEventLink(event.content["m.relates_to"].event_id, "[m.replace]")}</code>
+	} else {
+		return <code>{`${event.type}`}</code>
+	}
 }
 
 export default HiddenEvent
