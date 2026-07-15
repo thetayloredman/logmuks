6fad72f51585f7e5710be1dc0b92a146fe819281
Tulir Asokan <tulir@maunium.net>
2026-07-15 21:19:57 +0300

    web/settings: allow changing room name/topic

--- web/src/ui/settings/RoomSettings.tsx
+++ web/src/ui/settings/RoomSettings.tsx
@@ -13,13 +13,16 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
-import { use } from "react"
+import { use, useState } from "react"
+import { MoonLoader } from "react-spinners"
 import { getRoomAvatarThumbnailURL, getRoomAvatarURL } from "@/api/media.ts"
-import { RoomStateStore } from "@/api/statestore"
+import { RoomStateStore, useRoomState } from "@/api/statestore"
 import { RoomType } from "@/api/types"
 import { preferences } from "@/api/types/preferences"
 import { useEventAsState } from "@/util/eventdispatcher.ts"
+import { getEventLevel } from "@/util/powerlevel.ts"
 import ClientContext from "../ClientContext.ts"
+import { getPowerLevels } from "../menu/util.ts"
 import { LightboxContext, ModalCloseContext, ModalContext, modals } from "../modal"
 
 interface RoomSettingsProps {
@@ -31,6 +34,16 @@ const RoomSettings = ({ room }: RoomSettingsProps) => {
 	const client = use(ClientContext)!
 	const closeModal = use(ModalCloseContext)
 	const openModal = use(ModalContext)
+
+	useRoomState(room, "m.room.power_levels")
+	const [pls, ownPL] = getPowerLevels(room, client)
+	const canChangeName = ownPL >= getEventLevel(pls, "m.room.name", true)
+	const canChangeTopic = ownPL >= getEventLevel(pls, "m.room.topic", true)
+
+	const [newName, setNewName] = useState<string>()
+	const [newTopic, setNewTopic] = useState<string>()
+	const [loading, setLoading] = useState(false)
+
 	const onClickLeave = () => {
 		if (room && window.confirm(`Really leave ${room.meta.current.name}?`)) {
 			client.rpc.leaveRoom(room.roomID).then(
@@ -52,36 +65,86 @@ const RoomSettings = ({ room }: RoomSettingsProps) => {
 		window.mainScreenContext.setActiveRoom(previousRoomID!)
 		closeModal()
 	}
+	const cancelGeneralEdit = () => {
+		setNewName(undefined)
+		setNewTopic(undefined)
+	}
+	const saveGeneralInfo = () => {
+		if (loading) {
+			return
+		}
+		const promises = []
+		if (newName !== undefined && newName !== roomMeta.name) {
+			promises.push(client.rpc.setState(room.roomID, "m.room.name", "", { name: newName }))
+		}
+		if (newTopic !== undefined && newTopic !== roomMeta.topic) {
+			promises.push(client.rpc.setState(room.roomID, "m.room.topic", "", { topic: newTopic }))
+		}
+		if (promises.length) {
+			setLoading(true)
+			Promise.all(promises).then(
+				() => {
+					setNewName(undefined)
+					setNewTopic(undefined)
+				},
+				err => {
+					console.error("Failed to save room info", err)
+					window.alert(`Failed to save room info: ${err}`)
+				},
+			).finally(() => setLoading(false))
+		}
+	}
 	return <div className="room-details">
-		<img
-			className="avatar large"
-			loading="lazy"
-			src={getRoomAvatarThumbnailURL(roomMeta)}
-			data-full-src={getRoomAvatarURL(roomMeta)}
-			onClick={use(LightboxContext)}
-			alt=""
-		/>
-		<div>
-			{roomMeta.name && <div className="room-name">{roomMeta.name}</div>}
-			<code>{room!.roomID}</code>
-			<div>{roomMeta.topic}</div>
-			<div className="room-buttons">
-				<button className="leave-room" onClick={onClickLeave}>Leave room</button>
-				<button className="devtools" onClick={openDevtools}>Open devtools</button>
-				<select onChange={evt => {
-					window.activeRoomContext?.setForceViewType(evt.target.value as RoomType)
-					closeModal()
-				}} defaultValue="__null__">
-					{preferences.room_view_type.allowedValues!.map((val, i) =>
-						<option key={i} value={val ?? "__null__"} disabled={i === 0}>
-							{i === 0 ? "Override view" : preferences.room_view_type.valueLabels![i]}
-						</option>)}
-				</select>
-				{previousRoomID &&
-					<button className="previous-room" onClick={openPredecessorRoom}>
-						Open Predecessor Room
-					</button>}
+		<div className="general-info">
+			<img
+				className="avatar large"
+				loading="lazy"
+				src={getRoomAvatarThumbnailURL(roomMeta)}
+				data-full-src={getRoomAvatarURL(roomMeta)}
+				onClick={use(LightboxContext)}
+				alt=""
+			/>
+			<code>{room.roomID}</code>
+			<input
+				className="room-name-input"
+				type="text"
+				placeholder="Room name"
+				value={newName ?? roomMeta.name ?? ""}
+				onChange={evt => setNewName(evt.target.value)}
+				disabled={!canChangeName}
+			/>
+			<textarea
+				className="room-topic-input"
+				placeholder="Room topic"
+				value={newTopic ?? roomMeta.topic ?? ""}
+				onChange={evt => setNewTopic(evt.target.value)}
+				disabled={!canChangeTopic}
+			/>
+			<div className="buttons">
+				<button className="cancel-button" disabled={!newName && !newTopic} onClick={cancelGeneralEdit}>
+					Cancel
+				</button>
+				<button className="save-button" disabled={!newName && !newTopic} onClick={saveGeneralInfo}>
+					{loading ? <MoonLoader size={16} /> : "Save"}
+				</button>
 			</div>
+		</div>
+		<div className="room-buttons">
+			<button className="leave-room" onClick={onClickLeave}>Leave room</button>
+			<button className="devtools" onClick={openDevtools}>Open devtools</button>
+			<select onChange={evt => {
+				window.activeRoomContext?.setForceViewType(evt.target.value as RoomType)
+				closeModal()
+			}} defaultValue="__null__">
+				{preferences.room_view_type.allowedValues!.map((val, i) =>
+					<option key={i} value={val ?? "__null__"} disabled={i === 0}>
+						{i === 0 ? "Override view" : preferences.room_view_type.valueLabels![i]}
+					</option>)}
+			</select>
+			{previousRoomID &&
+				<button className="previous-room" onClick={openPredecessorRoom}>
+					Open Predecessor Room
+				</button>}
 		</div>
 	</div>
 }
